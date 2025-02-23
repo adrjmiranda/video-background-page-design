0dbba284ce05bc4a85117a13d339e4672ddbacf3
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-23 17:49:41 -0300

    style: added menu for smaller screens

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
 /**
+ * Toggle Navbar Menu
+ */
+const toggleMenuButton = document.getElementById(
+	'toggle-menu'
+) as HTMLButtonElement;
+const navbarMenu = document.querySelector('.navbar-menu') as HTMLUListElement;
+const navbarMenuLinks = document.querySelectorAll(
+	'.navbar-link'
+) as NodeListOf<HTMLAnchorElement>;
+
+if (toggleMenuButton && navbarMenu && navbarMenuLinks.length) {
+	toggleMenuButton.addEventListener('click', () => {
+		navbarMenu.classList.toggle('show');
+	});
+
+	navbarMenuLinks.forEach((link) =>
+		link.addEventListener('click', () => {
+			navbarMenu.classList.toggle('show');
+		})
+	);
+}
+
+/**
  * Scroll Reveal Animation
  */
 import ScrollReveal from 'scrollreveal';
