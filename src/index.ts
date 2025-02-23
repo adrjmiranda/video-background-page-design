dba26616de24dd52d10e6167b09cf4e9a6d49ae9
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-23 16:03:37 -0300

    chore: added animation to main title

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("It's working!");
+/**
+ * Scroll Reveal Animation
+ */
+import ScrollReveal from 'scrollreveal';
+
+ScrollReveal().reveal('.header-content', {
+	reset: false,
+	mobile: true,
+	opacity: 0,
+	origin: 'bottom',
+	distance: '30%',
+	easing: 'ease-in-out',
+	duration: 1750,
+	delay: 250,
+});
